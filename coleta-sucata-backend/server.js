8be16fce3bbe38408c83396fde959b495bc5932a
RogerReis <rogerreistec@gmail.com>
2024-10-29 14:16:34 -0300

    Primeiro commit - adicionando meu projeto

---
+++ coleta-sucata-backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const coletorRoutes = require('./routes/coletores');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/coletores', coletorRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
