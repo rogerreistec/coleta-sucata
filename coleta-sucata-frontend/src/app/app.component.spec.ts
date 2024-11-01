8f2f66a3e9f8b90a24fc384e3defb0b7f1adb806
RogerReis <rogerreistec@gmail.com>
2024-11-01 09:37:54 -0300

    Salvar progresso antes da atualização do Angular

--- coleta-sucata-frontend/src/app/app.component.spec.ts
+++ coleta-sucata-frontend/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterOutlet], // Incluindo RouterOutlet
     }).compileComponents();
   });
 
@@ -14,16 +15,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'coleta-sucata-frontend' title`, () => {
+  it(`should have the title 'coleta-sucata-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('coleta-sucata-frontend');
   });
 
-  it('should render title', () => {
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coleta-sucata-frontend');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy(); // Verifica se o <router-outlet> está presente
   });
 });
